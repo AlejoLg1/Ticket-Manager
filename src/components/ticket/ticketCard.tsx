1050c4da5efd710bed254cfa00a2f3dd874bc0be
Alejo Legname <legname.alejo@gmail.com>
2024-12-22 19:51:19 -0300

    Ticket card ReadOnly validations + styles

--- src/components/ticket/ticketCard.tsx
+++ src/components/ticket/ticketCard.tsx
@@ -2,6 +2,7 @@ import { Badge } from "@/components/ui/badges/badge";
 import { Card, CardContent } from "@/components/ui/cards/card";
 import { Button } from '@/components/ui/buttons/button';
 import EyeToggle from "@/components/eye/eyeToggle";
+import { TextArea } from "@ui/inputs/textArea";
 
 interface TicketCardProps {
   status: "nuevo" | "curso" | "desarrollo" | "contactado" | "finalizado";
@@ -13,6 +14,7 @@ interface TicketCardProps {
   role: "user" | "support";
   assignedUser: { name?: string; email?: string } | null;
   onAssign: () => void;
+  onClick: (ticketData: Omit<TicketCardProps, 'onClick'>) => void;
 }
 
 const AssignedLabel = ({ assignedUser }: { assignedUser: { name?: string; email?: string } | null }) => (
@@ -20,6 +22,8 @@ const AssignedLabel = ({ assignedUser }: { assignedUser: { name?: string; email?
     {assignedUser?.name || assignedUser?.email || "No asignado"}
   </div>
 );
+
+import { categoryOptions } from '@/constants/selectOptions';
 
 export function TicketCard({
   status,
@@ -31,6 +35,7 @@ export function TicketCard({
   role,
   assignedUser,
   onAssign,
+  onClick,
 }: TicketCardProps) {
   const statusStyles = {
     nuevo: "bg-green-100 text-green-800 w-[125px] h-[25px]",
@@ -47,6 +52,8 @@ export function TicketCard({
     contactado: "Contactado",
     finalizado: "Finalizado",
   };
+
+  const categoryLabel = categoryOptions.find(option => option.value === category)?.label || category;
 
   const renderAssignedSection = () => {
     if (role === 'support') {
@@ -65,7 +72,7 @@ export function TicketCard({
   };
 
   return (
-    <Card className="mb-4 w-[1300px] rounded-[55px] relative">
+    <Card className="mb-4 w-[1300px] !rounded-[40px] relative cursor-pointer hover:border-2 hover:border-[#CF230F]" onClick={() => onClick({ status, ticketNumber, contact, category, message, subject, role, assignedUser, onAssign })}>
       <CardContent className="p-4">
         <div className="flex flex-col h-full">
           <div className="flex justify-between items-start mb-4">
@@ -96,31 +103,32 @@ export function TicketCard({
               </ul>
               <ul className="list-disc ml-4">
                 <li className="flex items-center">
-                  <p className="text-sm font-bold text-black mt-3">Categoría:</p>
-                  <Badge variant="secondary" className="!bg-[#1A2F63] text-white mt-3 ml-2 whitespace-nowrap py-1">
-                    {category}
+                  <p className="text-sm font-bold text-black mt-4">Categoría:</p>
+                  <Badge variant="secondary" className="!bg-[#1A2F63] text-white mt-4 ml-2 whitespace-nowrap py-1">
+                    {categoryLabel}
                   </Badge>
                 </li>
               </ul>
             </div>
-            <div className="flex-1 ml-4">
-              <div
-                className="bg-white border border-black px-5 py-8 inline-block"
-                style={{
-                  borderRadius: '25px',
-                  width: '75%',
-                  height: '115px',
-                  paddingTop: '10px',
-                  paddingBottom: '50px',
-                }}
-              >
-                <p className="text-sm text-gray-600 text-left">{message}</p>
-              </div>
+
+            <div className="flex ml-4">
+              <TextArea
+                name="message"
+                placeholder="Escribe un mensaje..."
+                value={message}
+                onChange={() => {}}
+                readOnly={true}
+                className="border-gray-300 rounded-[15px] p-2 text-sm !h-[120px] !w-[700px]"
+              />
             </div>
+
           </div>
 
-          <div className="absolute bottom-4 right-14 flex flex-col items-start space-y-4" role="button" aria-label="Toggle eye visibility">
-            <EyeToggle fill="red" size={40} />
+          <div className="absolute bottom-6 right-14 flex flex-col items-start space-y-4" role="button" aria-label="Toggle eye visibility">
+            <EyeToggle 
+              fill="red" 
+              size={40}
+            />
           </div>
         </div>
       </CardContent>

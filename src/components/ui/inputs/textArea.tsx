6234728c5fe286bb692fd01debb1a871b94d9082
Alejo Legname <legname.alejo@gmail.com>
2024-12-22 19:52:05 -0300

    Components ReadOnly validations

--- src/components/ui/inputs/textArea.tsx
+++ src/components/ui/inputs/textArea.tsx
@@ -7,6 +7,7 @@ interface TextAreaProps {
   value: string;
   onChange: (value: string) => void;
   required?: boolean;
+  readOnly?: boolean;
   className?: string;
 }
 
@@ -16,6 +17,7 @@ export const TextArea = ({
   value,
   onChange,
   required,
+  readOnly,
   className,
 }: TextAreaProps) => {
   return (
@@ -25,6 +27,7 @@ export const TextArea = ({
       value={value}
       onChange={(e) => onChange(e.target.value)}
       required={required}
+      readOnly={readOnly}
       className={`w-full h-[150px] placeholder-gray-500 resize-none p-2 border rounded-[15px] focus:outline-none focus:border-gray-300 text-black ${className}`}
     />
   );

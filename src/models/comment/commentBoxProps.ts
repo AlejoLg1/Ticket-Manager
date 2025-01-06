b7d3826467e124af010f047ae5c2fb1b1882e1fc
Alejo Legname <legname.alejo@gmail.com>
2025-01-05 23:05:09 -0300

    Delete unnecesary file

--- src/models/comment/commentBoxProps.ts
+++
@@ -1,6 +0,0 @@
-interface CommentBoxProps {
-  isSupport: boolean;
-  ticketId: number;
-  onAddMessage: (newMessage: { id: string; text: string }) => void;
-  onDeleteMessage: (id: string) => void;
-}

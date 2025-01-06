849d2c9f93c45a0e1ea058239556f702d9ffe53b
Alejo Legname <legname.alejo@gmail.com>
2025-01-05 23:07:23 -0300

    Fix ESLint errors

--- src/components/ticket/ticketModal.tsx
+++ src/components/ticket/ticketModal.tsx
@@ -41,7 +41,7 @@ export default function BasicModal({
   const [message, setMessage] = useState<string>(ticket?.message || '');
   const [subject, setSubject] = useState<string>(ticket?.subject || '');
   const [notification, setNotification] = useState<string | null>(null);
-  const [_, setComments] = useState<{ id: string; text: string }[]>([]);
+  const setComments = useState<{ id: string; text: string }[]>([])[1];
   const isReadOnly = !isCreatingTicket && hasAssignment;
   const isEditable = isCreatingTicket || (!isCreatingTicket && !hasAssignment && !isSupport);
 

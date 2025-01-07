2395acf00117484a6bf3b4ec69a990e7e5c1a4a5
Alejo Legname <legname.alejo@gmail.com>
2025-01-07 16:03:09 -0300

    fix getSupportEmails query

--- src/app/api/services/support/support.ts
+++ src/app/api/services/support/support.ts
@@ -2,9 +2,9 @@ import pool from '@/lib/db';
 
 export const getSupportEmails = async (): Promise<string[]> => {
     const res = await pool.query(`
-      SELECT email
-      FROM supportwhitelist
-      ORDER BY email ASC;
+      SELECT u.email
+        FROM users u
+        INNER JOIN supportwhitelist sw ON u.id = sw.user_id;
     `);
   
     return res.rows.map(row => row.email);

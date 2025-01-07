f7c0cd2dc90571022ea2db8365eb8b8827202cfd
Alejo Legname <legname.alejo@gmail.com>
2025-01-06 22:19:24 -0300

    Set support role [TEST]

--- src/app/api/services/ticket/ticket.ts
+++ src/app/api/services/ticket/ticket.ts
@@ -28,7 +28,7 @@ export const getTickets = async (): Promise<Ticket[]> => {
     category: row.category_name,
     message: row.message,
     subject: row.subject,
-    role: 'user', // Pensar como hacerlo dinámico para reutilizar el endpoint -> Si es support aparece el botón de asignarme
+    role: 'support', // Pensar como hacerlo dinámico para reutilizar el endpoint -> Si es support aparece el botón de asignarme
     assignedUser: row.assignedtoid 
       ? { id: row.assignedtoid.toString(), email: row.assigned_email }
       : null,

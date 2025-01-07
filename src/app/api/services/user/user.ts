fc3dcb7cdbdecfdde12829040bc8b50a461115cf
Alejo Legname <legname.alejo@gmail.com>
2025-01-07 19:37:08 -0300

    fix query

--- src/app/api/services/user/user.ts
+++ src/app/api/services/user/user.ts
@@ -6,18 +6,16 @@ export const getUserByEmailAndPassword = async (
 ): Promise<{ id: string; email: string; role: string; name: string; lastName: string } | null> => {
   const res = await pool.query(
     `
-    SELECT id, email, role, name, lastname
+    SELECT id, email, role, first_name, last_name
     FROM users
-    WHERE email = $1 AND password = crypt($2, password)
+    WHERE email = $1 AND password = $2
     LIMIT 1;
     `,
     [email, password]
   );
-
   if (res.rows.length === 0) {
     return null;
   }
-
   const { id, email: userEmail, role, name, lastname } = res.rows[0];
   return { id: id.toString(), email: userEmail, role, name, lastName: lastname };
 };

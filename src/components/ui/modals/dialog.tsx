857567de3fdaac815be071490de145487a6eb825
Alejo Legname <legname.alejo@gmail.com>
2025-01-20 19:06:33 -0300

    Integrate confirmModal to assign/unassign

--- src/components/ui/modals/dialog.tsx
+++ src/components/ui/modals/dialog.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import '@/styles/states.css';
-import React, { ReactNode } from 'react';
+import React, { useState } from 'react';
 import { createPortal } from 'react-dom';
 import { Button } from '@/components/ui/buttons/button';
 import { Select } from '@/components/ui/selects/comboBox';
 import { statesOptions } from '@/constants/selectOptions';
+import ConfirmModal from '@/components/ui/modals/confirmModal';
 import useAuth from '@/hooks/useAuth';
-
 
 interface DialogProps {
   title: string;
@@ -15,7 +15,7 @@ interface DialogProps {
   onClose: () => void;
   isSupport: boolean;
   hasAssignment: boolean;
-  children: ReactNode;
+  children: React.ReactNode;
   selectedState: string;
   onStateChange: (newState: string) => void;
   ticketNumber: string;
@@ -33,6 +33,9 @@ export const Dialog = ({
   ticketNumber,
 }: DialogProps) => {
   const { session } = useAuth();
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+  const [action, setAction] = useState<'assign' | 'unassign' | null>(null);
+
   const handleAssign = async () => {
     const assignedId = Number(session?.user?.id);
     try {
@@ -74,66 +77,99 @@ export const Dialog = ({
     }
   };
 
+  const openConfirmModal = (actionType: 'assign' | 'unassign') => {
+    setAction(actionType);
+    setIsConfirmModalOpen(true);
+  };
+
+  const confirmModalClose = () => {
+    setIsConfirmModalOpen(false);
+    setAction(null);
+  };
+
+  const confirmModalAction = async (query: string) => {
+    if (action === 'assign') {
+      await handleAssign();
+    } else if (action === 'unassign') {
+      await handleUnassign();
+    }
+    confirmModalClose();
+  };
+
   if (!isOpen) return null;
 
   return createPortal(
-    <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
-      onClick={onClose}
-    >
+    <>
       <div
-        className="bg-[#F2F2F2] rounded-b-[25px] shadow-lg w-full max-w-4xl"
-        onClick={(e) => e.stopPropagation()}
+        className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+        onClick={onClose}
       >
-        <div className="bg-white p-4 shadow-lg rounded-t-md w-full flex justify-between items-center">
-          <h1 className="text-black text-2xl font-semibold">{title}</h1>
+        <div
+          className="bg-[#F2F2F2] rounded-b-[25px] shadow-lg w-full max-w-4xl"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="bg-white p-4 shadow-lg rounded-t-md w-full flex justify-between items-center">
+            <h1 className="text-black text-2xl font-semibold">{title}</h1>
 
-          {isSupport && (
-            <div className="flex items-center space-x-4">
-              <Select
-                options={statesOptions}
-                placeholder="Estado"
-                selected={statesOptions.find(option => option.value === selectedState) || null}
-                setSelected={(option) => {
-                  if (option) {
-                    onStateChange(option.value);
-                  }
-                }}
-                triggerClassName={`font-bold flex items-center justify-center rounded-full state-${selectedState} || state-nuevo`}
-                dropdownStyle={{
-                  borderRadius: '25px',
-                  overflowY: 'auto',
-                  maxHeight: '120px',
-                }}
-                itemClassName="hover:bg-gray-200"
-                hideXCircle={true}
-                hideSearchIcon={true}
-                hideChevronDown={false}
-                textClassName={`text-${selectedState} || text-nuevo`}
-              />
+            {isSupport && (
+              <div className="flex items-center space-x-4">
+                <Select
+                  options={statesOptions}
+                  placeholder="Estado"
+                  selected={statesOptions.find((option) => option.value === selectedState) || null}
+                  setSelected={(option) => {
+                    if (option) {
+                      onStateChange(option.value);
+                    }
+                  }}
+                  triggerClassName={`font-bold flex items-center justify-center rounded-full state-${selectedState} || state-nuevo`}
+                  dropdownStyle={{
+                    borderRadius: '25px',
+                    overflowY: 'auto',
+                    maxHeight: '120px',
+                  }}
+                  itemClassName="hover:bg-gray-200"
+                  hideXCircle
+                  hideSearchIcon
+                  hideChevronDown={false}
+                  textClassName={`text-${selectedState} || text-nuevo`}
+                />
 
-              <Button
-                className={`font-bold p-2 border rounded-[50px] w-[125px] h-[25px] flex items-center justify-center ${hasAssignment
-                  ? 'bg-[#504D4F] hover:bg-[#3D3B3C] text-white'
-                  : 'bg-[#0DBC2D] hover:bg-[#0B9E26] text-white'
-                }`}
-                onClick={() => {
-                  if (hasAssignment) {
-                    handleUnassign();
-                  } else {
-                    handleAssign();
-                  }
-                }}
-              >
-                {hasAssignment ? 'Desasignar' : 'Asignarme'}
-              </Button>
-            </div>
-          )}
+                <Button
+                  className={`font-bold p-2 border rounded-[50px] w-[125px] h-[25px] flex items-center justify-center ${
+                    hasAssignment
+                      ? 'bg-[#504D4F] hover:bg-[#3D3B3C] text-white'
+                      : 'bg-[#0DBC2D] hover:bg-[#0B9E26] text-white'
+                  }`}
+                  onClick={() => openConfirmModal(hasAssignment ? 'unassign' : 'assign')}
+                >
+                  {hasAssignment ? 'Desasignar' : 'Asignarme'}
+                </Button>
+              </div>
+            )}
+          </div>
+
+          <div className="p-6">{children}</div>
         </div>
-
-        <div className="p-6">{children}</div>
       </div>
-    </div>,
+
+      {isConfirmModalOpen && (
+        <ConfirmModal
+          isOpen={isConfirmModalOpen}
+          onClose={confirmModalClose}
+          onConfirm={confirmModalAction}
+          query={ticketNumber}
+          title={action === 'assign' ? 'Asignar ticket' : 'Desasignar ticket'}
+          text={
+            action === 'assign'
+              ? '¿Estás seguro de que deseas asignarte este ticket?'
+              : '¿Estás seguro de que deseas desasignar este ticket?'
+          }
+          confirmLabel= {action === 'assign'? 'Asignar' : 'Desasignar'}
+          cancelLabel="Cancelar"
+        />
+      )}
+    </>,
     document.body
   );
 };

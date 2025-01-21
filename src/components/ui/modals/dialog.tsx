f571bb2183a084f5b3d67f2761a8fab4a3d58c48
Alejo Legname <legname.alejo@gmail.com>
2025-01-21 18:10:34 -0300

    Blur for ticketModal

--- src/components/ui/modals/dialog.tsx
+++ src/components/ui/modals/dialog.tsx
@@ -103,7 +103,7 @@ export const Dialog = ({
   return createPortal(
     <>
       <div
-        className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+        className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm"
         onClick={onClose}
       >
         <div

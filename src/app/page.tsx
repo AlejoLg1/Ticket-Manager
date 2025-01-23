af8d47c15936f2e186a14d4ca02e1db27162214e
Alejo Legname <legname.alejo@gmail.com>
2025-01-22 23:40:16 -0300

    Integrate filters to user page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,38 +7,82 @@ import { NewTicketButton } from "@components/ticket/newTicketButton";
 import { TicketFilters } from "@/components/ticket/ticketFilters";
 import { TicketCard } from "@/components/ticket/ticketCard";
 import BasicModal from '@components/ticket/ticketModal';
+import HouseLoader from '@/components/loader/houseLoader';
 import { Ticket } from '@/models/ticket/ticket';
 import useAuth from '@/hooks/useAuth';
+
+interface Filters {
+  status: { value: string; label: string } | null;
+  ticketNumber: string;
+  assignedUser: { value: string; label: string } | null;
+  category: { value: string; label: string } | null;
+}
 
 const App = () => {
   const router = useRouter();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTicket, setSelectedTicket] = useState<Ticket | null>(null);
   const [tickets, setTickets] = useState<Ticket[]>([]);
+  const [isFetching, setIsFetching] = useState(false); // Loader control
   const { session } = useAuth();
-  
+  const [filters, setFilters] = useState<Filters>({
+    status: null,
+    ticketNumber: '',
+    assignedUser: null,
+    category: null,
+  });
+
   useEffect(() => {
     if (session && session.user.role !== 'user') {
       router.push('/login');
     }
   }, [session, router]);
 
-  useEffect(() => {
-    const fetchTickets = async () => {
-      try {
-        const query = session?.user?.id ? `?userId=${session.user.id}` : '';
-        const res = await fetch(`/api/services/ticket${query}`);
-        if (!res.ok) throw new Error('Error fetching tickets');
-        const data: Ticket[] = await res.json();
-        setTickets(data);
-      } catch (error) {
-        console.error('Error al cargar los tickets:', error);
+  const fetchTickets = async (filtersToUse: Filters) => {
+    setIsFetching(true);
+    try {
+      const queryParams = new URLSearchParams();
+
+      if (session?.user?.id) {
+        queryParams.append('userId', session.user.id);
       }
-    };
-  
-    fetchTickets();
+      if (filtersToUse.status) {
+        queryParams.append('status', filtersToUse.status.value);
+      }
+      if (filtersToUse.ticketNumber) {
+        queryParams.append('ticketNumber', filtersToUse.ticketNumber);
+      }
+      if (filtersToUse.assignedUser) {
+        queryParams.append('assignedUser', filtersToUse.assignedUser.value);
+      }
+      if (filtersToUse.category) {
+        queryParams.append('category', filtersToUse.category.value);
+      }
+
+      const res = await fetch(`/api/services/ticket?${queryParams.toString()}`);
+      if (!res.ok) throw new Error('Error fetching tickets');
+      const data: Ticket[] = await res.json();
+      setTickets(data);
+    } catch (error) {
+      console.error('Error al cargar los tickets:', error);
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
+  useEffect(() => {
+    if (session) {
+      fetchTickets(filters);
+    }
   }, [session]);
-  
+
+  const handleFilterApply = (newFilters: Filters) => {
+    setFilters((prevFilters) => {
+      const updatedFilters = { ...prevFilters, ...newFilters };
+      fetchTickets(updatedFilters);
+      return updatedFilters;
+    });
+  };
 
   const handleNewTicketClick = () => {
     setSelectedTicket(null);
@@ -66,12 +110,16 @@ const App = () => {
       </div>
 
       <div className="flex justify-center">
-        <TicketFilters />
+        <TicketFilters onFilterApply={handleFilterApply} />
       </div>
 
       <div className="flex justify-center mt-6">
         <div className="bg-[#EBEBEB] w-[90%] flex flex-col items-center justify-start rounded-[25px] shadow-md mb-6">
-          {tickets.length > 0 ? (
+          {isFetching ? (
+            <div className="flex justify-center items-center py-16">
+              <HouseLoader />
+            </div>
+          ) : tickets.length > 0 ? (
             tickets.map(ticket => (
               <div key={ticket.ticketNumber} className="w-[90%] p-6 pb-0">
                 <TicketCard
